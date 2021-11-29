fe8888051926e49d2c96ee0142a67905ba8b683c
damilola <damilolaakinterinwa@gmail.com>
2021-11-29 17:26:20 +0100

    Google play billing integration

--- app/src/main/java/com/aure/AdminProductDetails.java
+++ app/src/main/java/com/aure/AdminProductDetails.java
@@ -1,5 +1,7 @@
 package com.aure;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 import androidx.core.widget.NestedScrollView;
@@ -7,6 +9,7 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.PagerSnapHelper;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.Activity;
 import android.content.SharedPreferences;
 import android.os.Build;
 import android.os.Bundle;
@@ -16,11 +19,30 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.android.billingclient.api.BillingClient;
+import com.android.billingclient.api.BillingClientStateListener;
+import com.android.billingclient.api.BillingFlowParams;
+import com.android.billingclient.api.BillingResult;
+import com.android.billingclient.api.Purchase;
+import com.android.billingclient.api.PurchasesUpdatedListener;
+import com.android.billingclient.api.SkuDetails;
+import com.android.billingclient.api.SkuDetailsParams;
+import com.android.billingclient.api.SkuDetailsResponseListener;
 import com.aure.UiAdapters.MarketplaceProductDetailAdapter;
 import com.aure.UiModels.ListingModel;
 import com.google.android.material.button.MaterialButton;
+import com.limerse.iap.DataWrappers;
+import com.limerse.iap.IapConnector;
+import com.limerse.iap.PurchaseServiceListener;
+import com.limerse.iap.SubscriptionServiceListener;
+
+import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
 
 import me.relex.circleindicator.CircleIndicator2;
 
@@ -45,6 +67,62 @@ public class AdminProductDetails extends AppCompatActivity {
     }
 
     private void initView(){
+
+        List<String> nonConsumablesList = Collections.singletonList("aure.promote");
+        List<String> consumablesList = Arrays.asList("aure.promote");
+        List<String> subsList = Collections.singletonList("subscriptions");
+
+        IapConnector iapConnector = new IapConnector(
+                this,
+                nonConsumablesList,
+                consumablesList,
+                subsList,
+                  "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA2fDRqNLFSm7LYoCPZ/rG+8CpQXn/LCQNAxPtVRdt2ZNdpORpH0yvCm0vV8VOcSb6zWeM9s7dCt36wCLSJqllNw4fNkEWn/GcEV2iWNa3WT/I4JgDwstv4KGFq8FAYRA0Y+zICdvBUf833v/UuRWMAxUi2GfYmzJel+8uQtva1fzwHyzjRCYa1Od4F98IUebR0BfJ3Jp4KS3E5mr8GAuii61MxaR+n32YsEPC5gNCzkvpJO3PCbZr/XwiGG/l/sGPKQTEDapmLIhBOhnatwWj+Wmusww5RlsrEDwHnY6zRHQrwler1pW0IlqXzpyBDCKftGPa9N/o3KWof1WnUIGkXQIDAQAB",
+                  true
+        );
+
+
+        iapConnector.addPurchaseListener(new PurchaseServiceListener() {
+            public void onPricesUpdated(@NotNull Map iapKeyPrices) {
+
+            }
+
+            public void onProductPurchased(@NonNull DataWrappers.PurchaseInfo purchaseInfo) {
+                if (purchaseInfo.getSku().equals("plenty")) {
+
+                } else if (purchaseInfo.getSku().equals("yearly")) {
+
+                } else if (purchaseInfo.getSku().equals("moderate")) {
+
+                } else if (purchaseInfo.getSku().equals("base")) {
+
+                } else if (purchaseInfo.getSku().equals("quite")) {
+
+                }
+            }
+
+            public void onProductRestored(@NonNull DataWrappers.PurchaseInfo purchaseInfo) {
+
+            }
+        });
+        iapConnector.addSubscriptionListener(new SubscriptionServiceListener() {
+            public void onSubscriptionRestored(@NonNull DataWrappers.PurchaseInfo purchaseInfo) {
+            }
+
+            public void onSubscriptionPurchased(@NonNull DataWrappers.PurchaseInfo purchaseInfo) {
+                if (purchaseInfo.getSku().equals("subscription")) {
+
+                }
+            }
+
+            public void onPricesUpdated(@NotNull Map iapKeyPrices) {
+
+            }
+        });
+
+
+
+
         ignorePromotion = getIntent().getStringExtra("ignore");
         promote = findViewById(R.id.admin_product_sponsor);
         delete = findViewById(R.id.admin_product_delete);
@@ -66,6 +144,17 @@ public class AdminProductDetails extends AppCompatActivity {
         if(mListingModel.getIsSponsored().equalsIgnoreCase("true")){
             promote.setVisibility(View.GONE);
         }
+
+        promote.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                //iapConnector.subscribe(AdminProductDetails.this, "subscriptions");
+
+                iapConnector.purchase(AdminProductDetails.this, "aure.promote");
+
+            }
+        });
 
         if((ignorePromotion != null) && (!ignorePromotion.equalsIgnoreCase(""))){
             promote.setVisibility(View.GONE);
@@ -96,7 +185,6 @@ public class AdminProductDetails extends AppCompatActivity {
         });
     }
 
-
     @Override
     public void onResume() {
 
@@ -107,4 +195,5 @@ public class AdminProductDetails extends AppCompatActivity {
             getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR|View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
         }
     }
+
 }
